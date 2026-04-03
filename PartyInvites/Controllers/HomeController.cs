8e532439c296d0ae2a775517047a9a6f70f6d6e2
chaibaojie <chaibaojie@126.com>
2026-04-03 16:46:38 +0800

    完成P.46, listing3-16

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -18,7 +18,11 @@ public class HomeController : Controller
     [HttpPost]
     public ViewResult RsvpForm(GuestResponse guestResponse)
     {
-        //TODO: Store response from guest 
-        return View();
+        Repository.AddResponse(guestResponse);
+        return View("Thanks", guestResponse);
     }     
+    public ViewResult ListResponses()
+    {
+        return View(Repository.Responses.Where(r => r.WillAttend == true));
+    }
 }
